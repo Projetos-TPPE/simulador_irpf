6552b910d443521ccb9354bab0c605bc1bc5a1e1
codehg <hugoaraliveira@gmail.com>
2022-02-24 20:39:24 -0300

    Versao corrigida parametrizacao CalculaBase
    Co-authored-by:francisco1code<francisco1code@gmail.com>

--- src/tst/CalculaBaseTeste.java
+++ src/tst/CalculaBaseTeste.java
@@ -2,36 +2,78 @@ package tst;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import app.CalculaBase;
 
+@RunWith(Parameterized.class)
 public class CalculaBaseTeste {
 	
 	CalculaBase calcula;
+	Object[][] base;
+	float valorEsperado;
 		
 	@Before
 	public void setup() {
 		calcula = new CalculaBase();
 	}
 	
-	@Test
-	public void testSubtracao() {
-		calcula.subtracao(10000f, 2000f);
-		assertEquals(8000f, calcula.getTotalBase(), 0f);
+	public CalculaBaseTeste(Object[][] base, float valorEsperado) {
+		this.base = base;
+		this.valorEsperado = valorEsperado;
+	}
+	
+	@Parameters
+	public static Collection<Object[]> getParamters(){
+		// {{{ valorRendimentos, valorDeducoes}} , valorEsperado}
+		// {{
+		//   {10000f, 2000f}, 8000f}
+		Object[][] resultado = new Object[][] {
+			{new Object[][] {
+				{10000f, 2000f}
+			}, 8000f},
+			{new Object[][] {
+				{10000f, 5000f}
+			}, 5000f},
+			{new Object[][] {
+				{12345f, 12000f},
+			}, 345f},
+		};
+		
+		return Arrays.asList(resultado);
 	}
 	
 	@Test
-	public void testSubtracaoDupla() {
-		calcula.subtracao(10000f, 5000f);
-		assertEquals(5000f, calcula.getTotalBase(), 0f);
+	public void testCalculoBase() {
+		for (Object[] o : base) {
+			calcula.subtracao((float)o[0], (float)o[1]);
+			assertEquals(valorEsperado, calcula.getTotalBase(), 0f);
+		}
 	}
 	
-	@Test
-	public void testSubtracaoTrian() {
-		calcula.subtracao(12345f, 12000f);
-		assertEquals(345f, calcula.getTotalBase(), 0f);
-	}
+//	@Test
+//	public void testSubtracao() {
+//		calcula.subtracao(10000f, 2000f);
+//		assertEquals(8000f, calcula.getTotalBase(), 0f);
+//	}
+//	
+//	@Test
+//	public void testSubtracaoDupla() {
+//		calcula.subtracao(10000f, 5000f);
+//		assertEquals(5000f, calcula.getTotalBase(), 0f);
+//	}
+//	
+//	@Test
+//	public void testSubtracaoTrian() {
+//		calcula.subtracao(12345f, 12000f);
+//		assertEquals(345f, calcula.getTotalBase(), 0f);
+//	}
 		
 }
