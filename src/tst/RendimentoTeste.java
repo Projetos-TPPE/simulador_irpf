4aa41d7f686997dbb3b08a7b2348159f67d20412
Bruno-Felix <balvesfelix@gmail.com>
2022-02-25 01:19:49 -0300

    Comentando Old Tests Rendimentos

--- src/tst/RendimentoTeste.java
+++ src/tst/RendimentoTeste.java
@@ -58,4 +58,27 @@ public class RendimentoTeste {
 		}
 		assertEquals(10000f, rendimento.getTotalRendimentos(), 0f);
 	}
+	
+	/*@Test
+    public void testeRendimento() {
+    	rendimento.cadastrarRendimento("Salario", 1000f);
+        assertEquals(1000f, rendimento.getTotalRendimentos(), 0f);
+    }*/
+
+    /*@Test
+    public void testeRendimento2() {
+    	System.out.print(rendimento.getTotalRendimentos());
+    	rendimento.cadastrarRendimento("Salario", 1000f);
+    	rendimento.cadastrarRendimento("Salario", 2000f);
+        assertEquals(3000f, rendimento.getTotalRendimentos(), 0f);
+    }*/
+
+    /*@Test
+    public void testeRendimento3() {
+    	System.out.print(rendimento.getTotalRendimentos());
+    	rendimento.cadastrarRendimento("Salario", 1000f);
+    	rendimento.cadastrarRendimento("Salario", 2000f);
+    	rendimento.cadastrarRendimento("Salario", 3000f);
+        assertEquals(6000f, rendimento.getTotalRendimentos(), 0f);
+    }*/
 }
