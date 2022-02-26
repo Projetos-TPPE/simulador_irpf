e37bc73e9be62a73ff5dfcc005ae714907d9414c
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2022-02-25 21:52:50 -0300

    Testes de exceção - Rendimentos
    Co-authored-by: edsondearaujo <edsondearaujofga@gmail.com>
    Co-authored-by: enzoggqs <eggqsaraiva@gmail.com>
    Co-authored-by: Bruno-Felix <balvesfelix@gmail.com>

---
+++ src/app/DescricaoEmBrancoException.java
@@ -0,0 +1,10 @@
+package app;
+
+public class DescricaoEmBrancoException extends IllegalArgumentException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DescricaoEmBrancoException(String message) {
+		super(message);
+	}
+}
--- src/app/Rendimento.java
+++ src/app/Rendimento.java
@@ -5,6 +5,12 @@ public class Rendimento {
 	private float totalRendimentos;
 	
 	public void cadastrarRendimento(String descricao, float valorRendimento) {
+		if(descricao == "" || descricao == null) {
+			throw new DescricaoEmBrancoException("Não é permitido exceção em branco!");
+		}
+		if(valorRendimento < 0) {
+			throw new ValorRendimentoInvalidoException("Não é permitido exceção em branco!");
+		}
 		this.totalRendimentos += valorRendimento;
 	}
 
