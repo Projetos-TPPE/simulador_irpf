e76fb8cb8617a9a8563cc9985df410ebd5eaf326
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-04-24 20:20:40 -0300

    Refactoring: Extrair constante Mau-cheiro identificado: variáveis "hardcodadas" no código; Descrição da refatoração: aplicou-se a técnica de transformar as variáveis "hardcodadas" do código em constantes da classe CalculadoraImposto. * Classe CalculadoraImposto foi alterada: Agora as variáveis "hardcodadas" foram convertidas em constantes para serem chamadas no método calcula() da Classe CalculadoraImposto. Melhorando assim novas implementações e manutenção dessas constantes. * Classe CalculadoraImposto foi alterada: Os atributos extraídos de calculaImposto() foram transformados em constantes da classe CalculadoraImposto.
    Co-authored-by: edsondearaujo <edsondearaujofga@gmail.com>
    Co-authored-by: Bruno-Felix<balvesfelix@gmail.com>
    Co-authored-by: eliseukadesh67<eliseu.kadesh1967@gmail.com>
    Co-authored-by: francisco1code<francisco1code@gmail.com>

--- src/app/CalculadoraImposto.java
+++ src/app/CalculadoraImposto.java
@@ -1,67 +1,73 @@
 package app;
 
 public class CalculadoraImposto {
-
-    //Referencia para o objeto fonte
+	
+	//Referencia Objeto Imposto
     Imposto fonte;
 
-    //Variaveis temporarias/atributo
+	//Constantes Extraídas
+    private static final double AliquotaFaixa5 = 0.275;
+	private static final double AliquotaFaixa4 = 0.225;
+	private static final double AliquotaFaixa3 = 0.15;
+	private static final double AliquotaFaixa2 = 0.075;
+	private static final int AliquotaFaixa1 = 0;
+	
+	//Atributos Método Objeto
+    private static final float faixa1 = 1903.98f;
+    private static final float faixa2 = 922.67f;
+    private static final float faixa3 = 924.40f;
+    private static final float faixa4 = 913.63f;
+    
     private float baseCalculo;
     private float impostoTotal;
-
-    //Valor fixo
-    float faixa1 = 1903.98f;
-    float faixa2 = 922.67f;
-    float faixa3 = 924.40f;
-    float faixa4 = 913.63f;
-
+    
     public CalculadoraImposto(Imposto fonte, float baseCalculo) {
         this.fonte = fonte;
         this.baseCalculo = baseCalculo;
     }
 
     public void calcular() {
-        if (baseCalculo <= faixa1) {
-            impostoTotal += baseCalculo * 0;
-            baseCalculo -= faixa1;
-        }
+    	if (baseCalculo <= faixa1) {
+    		impostoTotal += baseCalculo * AliquotaFaixa1;
+    		baseCalculo -= faixa1;
+    	}
 
-        if (baseCalculo > faixa1) {
-            baseCalculo -= faixa1;
-        }
+    	if (baseCalculo > faixa1) {
+    		baseCalculo -= faixa1;
+    	}
 
-        if (baseCalculo > 0 && baseCalculo <= faixa2) {
-            impostoTotal += baseCalculo * 0.075;
-            baseCalculo -= faixa2;
-        }
+		if (baseCalculo > 0 && baseCalculo <= faixa2) {
+		    impostoTotal += baseCalculo * AliquotaFaixa2;
+		    baseCalculo -= faixa2;
+		}
 
         if(baseCalculo > faixa2) {
-            impostoTotal += faixa2 * 0.075;
+            impostoTotal += faixa2 * AliquotaFaixa2;
             baseCalculo -= faixa2;
         }
 
         if (baseCalculo > 0 && baseCalculo < faixa3) {
-            impostoTotal += baseCalculo * 0.15;
+            impostoTotal += baseCalculo * AliquotaFaixa3;
             baseCalculo -= faixa3;
         }
 
         if (baseCalculo > faixa3) {
-            impostoTotal += faixa3 * 0.15;
+            impostoTotal += faixa3 * AliquotaFaixa3;
             baseCalculo -= faixa3;
         }
 
         if (baseCalculo > 0 && baseCalculo < faixa4) {
-            impostoTotal += baseCalculo * 0.225;
+            impostoTotal += baseCalculo * AliquotaFaixa4;
             baseCalculo -= faixa4;
         }
 
         if (baseCalculo > faixa4) {
-            impostoTotal += faixa4 * 0.225;
+            impostoTotal += faixa4 * AliquotaFaixa4;
             baseCalculo -= faixa4;
         }
 
         if (baseCalculo > 0) {
-            impostoTotal += baseCalculo * 0.275;
+            impostoTotal += baseCalculo * AliquotaFaixa5;
         }
 
         fonte.setImposto(impostoTotal);
